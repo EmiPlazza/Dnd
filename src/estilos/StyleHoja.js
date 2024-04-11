5005820fd3d9da0de0c0a9d39794918a1de73844
privada <plazafau@gmail.com>
2024-04-11 15:38:34 -0300

    volviendo a volarlo mas a la mierda

---
+++ src/estilos/StyleHoja.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native'; 
+import { useTheme } from 'react-native-paper';
+
+
+
+const StyleHoja = () => {
+    const theme = useTheme(); 
+
+    const styles = StyleSheet.create({
+     
+});
+
+    return styles; 
+};
+
+export default  StyleHoja
---
+++ src/estilos/StyleMain.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native'; 
+import { useTheme } from 'react-native-paper';
+
+
+
+const StyleMain = () => {
+    const theme = useTheme(); 
+
+    const styles = StyleSheet.create({
+     
+});
+
+    return styles; 
+};
+
+export default  StyleMain
